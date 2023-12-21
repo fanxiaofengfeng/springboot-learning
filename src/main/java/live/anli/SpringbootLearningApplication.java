59260ebee72895fba2f2c8697715a54690916acb
fanxiaofeng <fanxiaofeng@qianlima.com>
2023-12-21 19:26:35 +0800

    初始化

---
+++ src/main/java/live/anli/SpringbootLearningApplication.java
@@ -0,0 +1,13 @@
+package live.anli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/live/anli/SpringbootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package live.anli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
