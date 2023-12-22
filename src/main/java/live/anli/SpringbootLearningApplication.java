0f7f42141d9d00394da955c4951458c83d3dd847
fanxiaofeng <fanxiaofeng@qianlima.com>
2023-12-22 14:24:46 +0800

    添加源码

--- src/main/java/live/anli/SpringbootLearningApplication.java
+++ src/main/java/live/anli/SpringbootLearningApplication.java
@@ -1,13 +1,19 @@
 package live.anli;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootLearningApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootLearningApplication.class, args);
+//        ConfigurableApplicationContext run = SpringApplication.run(SpringbootLearningApplication.class, args);
+         SpringApplication app = new SpringApplication(SpringbootLearningApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+
     }
 
 }
---
+++ src/main/java/live/anli/controller/HelloController.java
@@ -0,0 +1,16 @@
+package live.anli.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping("/world")
+    @ResponseBody
+    public String hello() {
+        System.out.println("======hello world=======");
+        return "hello";
+    }
+}
